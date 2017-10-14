58c485fd0cf1f6900de5631a5f128d7dd1850b34
Bruno Petrus <bruno.petrus@live.com>
2017-10-14 15:01:09 +0100

    Prve riesenia

--- 5. Spravne poradie/riesenie.cpp
+++ 5. Spravne poradie/riesenie.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <map>
+#include <vector>
+
 
 using namespace std;
 
@@ -8,21 +11,50 @@ bool** pravidla = nullptr;
 int main() {
     cin >> nAkcii >> nZavislosti >> nBojovnikov;
 
-    // Vytvorit tabulku s pravidlami
-    pravidla = new bool* [nAkcii];
-    for(int i = 0; i < nAkcii; i++) {
-        pravidla[i] = new bool[nAkcii];
-        for (int j = 0; j < nAkcii; j++) {
-            pravidla[i][j] = false;
+    map<int, vector<int>> pravidla;
+
+    // Vytvorime pravidla
+    int key = 0;
+    int value = 0;
+    for (int i = 0; i < nZavislosti; i++) {
+        cin >> key >> value;
+        pravidla[i].push_back(value);
+    }
+
+    // Iterujeme cez jednotlivych bojovnikov
+    for (int i = 0; i < nBojovnikov; i++) {
+        int akcneBody = 0;
+        cin >> akcneBody;
+        map<int, bool> bojovnik;
+        for (int j = 0; j < akcneBody; j++) {
+            int a = 0;
+            cin >> a;
+            bojovnik[a] = true;
         }
-    } 
 
+        while (akcneBody > 0) {
+            for (auto k : bojovnik) {
+                bool mozmeVymazat = true;
 
-    // Vymaz tabulku s pravidlami
-    for(int i = 0; i < nAkcii; i++) {
-        delete[] pravidla[i];
-    } 
-    delete[] pravidla;
+                // Pozri sa na pravidla
+                //vector<int> momentalnePravidla = pravidla[k]; 
+                for (auto pravidlo : pravidla[k.first]) {
+                    // Ak by sme urobenim akcie zabranili urobeniu dalsich
+                    if (bojovnik[pravidlo] == true) {
+                        mozmeVymazat = false;
+                        break;
+                    }
+                }
 
+                if (mozmeVymazat) {
+                    // Zbytocna medzera na konci by mohla sposobit chybu
+                    cout << k.first << " ";
+                    bojovnik.erase(k.first);
+                    akcneBody--;
+                }
+            }
+            cout << "\n";
+        }
+    }
     return 0;
 }
