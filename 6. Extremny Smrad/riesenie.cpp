2735029c28d9634e8b2a7539c6ad5f658e0ff117
Bruno Petrus <bruno.petrus@live.com>
2017-10-16 12:08:53 +0100

    6. problem
    Este nedoriesenie k

--- 6. Extremny Smrad/riesenie.cpp
+++ 6. Extremny Smrad/riesenie.cpp
@@ -1,45 +1,74 @@
 #include <iostream>
+
 using namespace std;
-double** mapa = nullptr;double** riesenie = nullptr;int n = 0, k = 0;
-double najdiNajvacsie(int i, int j) {
-    double najvacsie = -1;   
-    // Vrchny rad
-    if (mapa[i-1][j-1] > najvacsie) najvacsie = mapa[i-1][j-1];
-    if (mapa[i-1][j] > najvacsie) najvacsie = mapa[i-1][j];
-    if (mapa[i-1][j+1] > najvacsie) najvacsie = mapa[i-1][j+1];
-    // Stredny rad
-    if (mapa[i][j-1] > najvacsie) najvacsie = mapa[i][j-1];
-    if (mapa[i][j] > najvacsie) najvacsie = mapa[i][j];
-    if (mapa[i][j+1] > najvacsie) najvacsie = mapa[i][j+1]; 
-    // Spodny rad 
-    if (mapa[i+1][j-1] > najvacsie) najvacsie = mapa[i+1][j-1];
-    if (mapa[i+1][j] > najvacsie) najvacsie = mapa[i+1][j];
-    if (mapa[i+1][j+1] > najvacsie) najvacsie = mapa[i+1][j+1];
-    return najvacsie; 
+
+long** mapa = nullptr;
+int n = 0, k = 0;
+
+long najdiNajvacsie(int i, int j) {
+    long najvacsie = -1;
+    // // Vrchny rad
+    // if (mapa[i-1][j-1] > najvacsie) najvacsie = mapa[i-1][j-1];
+    // if (mapa[i-1][j] > najvacsie) najvacsie = mapa[i-1][j];
+    // if (mapa[i-1][j+1] > najvacsie) najvacsie = mapa[i-1][j+1];
+    
+    // // Stredny rad
+    // if (mapa[i][j-1] > najvacsie) najvacsie = mapa[i][j-1];
+    // if (mapa[i][j] > najvacsie) najvacsie = mapa[i][j];
+    // if (mapa[i][j+1] > najvacsie) najvacsie = mapa[i][j+1];
+    
+    // // Spodny rad
+    // if (mapa[i+1][j-1] > najvacsie) najvacsie = mapa[i+1][j-1];
+    // if (mapa[i+1][j] > najvacsie) najvacsie = mapa[i+1][j];
+    // if (mapa[i+1][j+1] > najvacsie) najvacsie = mapa[i+1][j+1];
+    
+    for (int x = i-1; x <= i+1; x++) {
+        if ( x <= 0 || x > n-1 )
+            continue;
+        for (int y = j-1; y <= j+1 ; y++) {
+            if ( y <= 0 || y > n-1 )
+                continue;
+            if (mapa[x][y] > najvacsie)
+                najvacsie = mapa[x][y];
+        }
+    }
+    
+    return najvacsie;
+    
 }
 
-int main() {   
+int main() {
     // Vstup
     cin >> n >> k;
+    
     // Mapa
-    mapa = new double*[n];
-    riesenie = new double*[n];
+    mapa = new long*[n];
+   // riesenie = new double*[n];
     for (int i = 0; i < n; i++) {
-        mapa[i] = new double[n];
-        riesenie[i] = new double[n];
+        mapa[i] = new long[n];
     }
-    // Nacitaj mapu
+
+    // Nacitaj mapu
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-             cin >> mapa[i][j];
+       for (int j = 0; j < n; j++) {
+            cin >> mapa[i][j];
         }
-    }                
-    // Vymazanie mapy    
+    }
+    
+    // Riesenie
+    for (int i = 0; i < n; i++) {
+       for (int j = 0; j < n; j++) {
+            cout << najdiNajvacsie(i, j);
+            // cout << mapa[i][j];
+        }
+        cout << "\n";
+    }
+    
+    // Vymazanie mapy
     for (int i = 0; i < n; i++) {
         delete[] mapa[i];
-        delete[] riesenie[i];
-    }    
-    delete[] mapa;    
-    delete[] riesenie;        
-    return 0;    
+    }
+    delete[] mapa;
+    
+    return 0;    
 }
